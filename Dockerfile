c1db4af4e154f33f1a55dbf0072f03344fb67750
Oscar Bastardo <osalbr.14@gmail.com>
2018-08-15 01:52:40 +0100

    setup basic task queue with celery

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM python:3.4-alpine
 ADD . /code
 WORKDIR /code
 RUN pip install -r requirements.txt
-CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-import time
-
-import redis
-from flask import Flask
-
+from flask import Flask, request, render_template, session, flash, redirect, url_for, jsonify
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
+app.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379/0'
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+from celery import Celery
 
+def make_celery(app):
+    celery = Celery(
+        app.name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
+@celery.task()
+def add_together(a, b):
+    return a + b
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+def root():
+    result = add_together.delay(23, 42)
+    result.wait()  # 65
+    return jsonify({'result': result.get()})
+
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
